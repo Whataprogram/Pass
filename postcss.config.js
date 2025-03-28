c19e7ac7c5a6c525bdfef3dc08726af2bce924f3
bradfordr999 <bradfordr999@gmail.com>
2025-03-27 22:34:58 -0400

    commit package-lock.json commit package.json commit postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,11 @@
-export default {
+module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
+  "scripts": {
+    "dev": "vite",
+    "build": "vite build",
+    "serve": "vite preview"
+  }
 };
